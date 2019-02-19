2566946441e15dd03b04a5133b26b6dfc43b6957
Shayla <itsshaylan@gmail.com>
2019-02-18 19:42:22 -0600

    add existing file

---
+++ server.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+
+  fs.writeFile('hello-world.txt', 'Hello to this great world', (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log(`File is created`);
+  });
+  response.end(`Handling a request on port ${port}`)
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error: ${err}`);
+  }
+  return console.log(`Server is listening on port: ${port}`);
